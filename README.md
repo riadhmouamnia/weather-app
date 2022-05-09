45b9d1f398abdfd36532c08c438a16fb4963e395
Riadh mouamnia <riad.sodmg@gmail.com>
2022-05-10 00:57:06 +0100

    firstCommit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # weather-app
 creating weather app using HTML, CSS, and JavaScript
+
+![Tux, the Linux mascot](app.png)
---
+++ app.js
@@ -0,0 +1,41 @@
+const search = document.querySelector('form');
+const weatherDetails = document.querySelector('.weather')
+const updateCity = async (city) => {
+    const weather = await fetchWeather(city);
+    return weather;
+}
+
+const updateUi = (data) => {
+    const weather = data;
+    // get the city weather details 
+    const {name} = data;
+    const {icon, description} = data.weather[0];
+    const {temp, humidity} = data.main;
+    const {speed} = data.wind;
+    // update weather details template
+    weatherDetails.innerHTML = 
+    `
+    <div>
+    <h2>${name}</h2>
+    <div class="temp">${temp}°C</div>
+    <article class="weather-description">
+      <p class="description">
+      <img src="https://openweathermap.org/img/wn/${icon}.png" alt="weather icon" class="icon" />
+      ${description}y</p>
+      <p class="humidity">${humidity}%</p>
+      <p class="wind">${speed}km/h</p>
+    </article></div>    
+    `
+    weatherDetails.classList.remove('hidden');
+};
+
+search.addEventListener('submit', e => {
+    //prevent from default action
+    e.preventDefault();
+    const city = search.city.value.trim();
+    search.reset();
+    //update the UI with new city
+    updateCity(city)
+    .then(data => updateUi(data))
+    .catch(err => console.log(err));
+});
